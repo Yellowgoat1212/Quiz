c2ebe085149c5d2f30fdf42bf3c05830cb21f96e
Yellowgoat1212 <78886208+Yellowgoat1212@users.noreply.github.com>
2021-05-12 14:46:17 +1200

    User_details

---
+++ User_detail.py
@@ -0,0 +1,28 @@
+#This program will get user details such as name and age. 
+
+#In this code i'll be defining the user_details function with one pameter 
+
+def user_details(name):
+    greet = name
+    print("Hello",greet)
+
+#In this code it'll accept lower case but won't accept invalid answers 
+while True:
+    name = input ("Enter your name : ")
+    if name.isalpha():
+        break
+    print("Please enter character A-Z only")
+print("Hey",name)
+
+
+#this code is going to be similar to name but won't accept lower case and won't accept alphabet
+while True:
+    age = input ("Enter your age : ")
+    if age.isnumeric():
+        break
+    print("Please enter numbers 5-80 only")
+
+
+user_details(name)
+print("nice your",age,"awsome lets get right to the quiz.")
+
