bd8ad842dc405ca9487f918a63797a9a994260d2
Yellowgoat1212 <78886208+Yellowgoat1212@users.noreply.github.com>
2021-05-12 14:47:25 +1200

    Instruction/Rules

---
+++ Rules_Quiz.py
@@ -0,0 +1,13 @@
+#This version will give user muiltple choice to choose if they want rules or no
+print("===========================================================================================================================")
+print("Welcome to NBA Quiz program. Program will be a multichoice quiz giving you option answers to choose from. Please follow the instructions and enjoy playing the quiz.")
+print("===========================================================================================================================")
+
+def instructions():
+    inst=input("Hey would like to read the instruction Enter Yes or NO : ").lower()
+    if inst == 'Yes' or inst == 'YES' or inst == 'Y' or inst == 'ye':
+        print("Once you enter the quiz you'll have multiple choice to choose from A,B,C and D")
+        print("Read the questions carefully and answer corretly Good Luck")
+    else:
+        print("Ok. Lets begin the game.")
+instructions()
