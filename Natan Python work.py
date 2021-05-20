ccf1636fb25db4a5c4133cf2c85a297c6dfd8c29
Yellowgoat1212 <78886208+Yellowgoat1212@users.noreply.github.com>
2021-05-20 14:11:43 +1200

    Update Natan Python work.py

--- Natan Python work.py
+++ Natan Python work.py
@@ -5,6 +5,8 @@ Q2 = 'Toronto'
 Q3 = 'Micheal Jordan'
 Q4 = 'Kareem Abdul-Jabbar'
 Q5 = 'Ray Allen'
+Q6 = 'Russel Westbrook'
+
 
 score = 0
 #Ask for name
@@ -24,8 +26,15 @@ while True:
     print("Please enter numbers 5-80 only")
 print(age)
 
-#Ask for year level
-yrlvl = int(input("Enter your year level: "))
+#This code is if the end user need instrucation 
+inst=input("Hey would like to read the instruction Enter Yes or NO : ").lower()
+
+if inst ==  'Y' or inst  ==  'y' or  inst == 'Yes' or inst == 'yes':
+    print("Once you enter the quiz you'll have multiple choice to choose from A,B,C and D")
+    print("This Quiz is based on National Basketball Asscositon so you'll be questioned about the history and general knowledage behind the National Basketball Association")
+    print("Last but no least ,read the questions carefully and answer the questions corretly Good Luck")   
+else:
+    print("Let start")
 
 #Ask if they are ready to take the quiz
 status = input("Are you ready to take the quiz :{}?:  \na. Yes \nb. No \n=>".format(name))
@@ -33,32 +42,35 @@ status = input("Are you ready to take the quiz :{}?:  \na. Yes \nb. No \n=>".for
 # what if the the user is not ready
 if status == 'No' or status == 'no' or status == 'n' or status == 'N' :
    print("See you next time.")
+   exit()
 # what if the user is ready?
 if status == 'Yes' or status == 'yes' or status == 'y' or status == 'Y' or status == 'Ye' or status == 'ye' or status == 'A' or status == 'a':
-    print("Welcome to the quiz.")
+    print("======================================")
+    print("Welcome to NBA Quiz program.Enjoy .")
+    print("======================================")
 
 # set of questions that are asked
 #question 1
 
-    print("\nQuestion: 1 | score: {}".format(score))
-    ans=input("Who was the Founder of the Nation Basketball Association?\na.Lavar Ball\nb.Walter Brown\nc.Stephen A.Smith : ").upper()
-    if ans == 'WALTER BROWN' or ans == 'B':
-        print('Awesome Correct')
-        score +=2
-    else:
-       print("incorrect it's" ,Q1)
-       print("better luck next Question ¯\_(ツ)_/¯ ")
-       if score <=0:
-            score =0
+print("\nQuestion: 1 | score: {}".format(score))
+ans=input("Who was the Founder of the Nation Basketball Association?\na.Lavar Ball\nb.Walter Brown\nc.Stephen A.Smith : ").upper()
+if ans == 'WALTER BROWN' or ans == 'B':
+    print('Awesome Correct')
+    score +=1
+else:
+    print("incorrect it's" ,Q1)
+    print("incorret  ")
+    if score <=0:
+        score =0
         
 
 #Question 2
             
     print("\nQuestion: 2 | score: {}".format(score))
-    ans=input("Where was the first NBA game conducted?\na.Chicage\nb.Washington DC\nc.Toronto : ").upper()
+    ans=input("Where was the first NBA game conducted?\na.Chicago\nb.Washington DC\nc.Toronto : ").upper()
     if ans == 'TORONTO' or ans == 'C':
         print('Correct')
-        score +=2
+        score +=1
     else:
         print("incorrect it's" ,Q2)
         if score <=0:
@@ -70,7 +82,7 @@ if status == 'Yes' or status == 'yes' or status == 'y' or status == 'Y' or statu
     ans=input("What player was named the G.O.A.T(Greatest of all time) that played for the Chicago Bulls in 1995?\na.Dennis Rodman\nb.Micheal Jordan\nc.Sccotie Pippen\nd.Derrick Rose : ")
     if ans == 'Micheal Jordan' or ans == 'micheal jordan'or ans == 'B' or ans == 'b':
         print('Correct')
-        score +=2
+        score +=1
     else:
         print("incorrect it's" ,Q3)
         if score <=0:
@@ -83,7 +95,7 @@ if status == 'Yes' or status == 'yes' or status == 'y' or status == 'Y' or statu
     ans=input("What player is leading the NBA ALL-Time scoring?\na.Lebron James\nb.Kareem Abdul-Jabbar\nc.Karl Malone\nd.Kobe Bryant : ")
     if ans == 'Kareem Abdul-Jabbar' or ans == 'kareem abdul-jabbar'or ans == 'B' or ans == 'b':
         print('Correct')
-        score +=2
+        score +=1
     else:
         print("incorrect it's" ,Q4)
         if score <=0:
@@ -96,15 +108,26 @@ if status == 'Yes' or status == 'yes' or status == 'y' or status == 'Y' or statu
     ans=input("Who’s leading the NBa all-time 3 pointers made in the NBA right now?\na.Stephen Curry\nb.Ben Simmons\nc.Ray Allen\nd.Reggie Miller : ")
     if ans == 'Ray Allen' or ans == 'ray allen'or ans == 'C' or ans == 'c':
         print('Correct')
-        score +=2
+        score +=1
     else:
         print("incorrect it's" ,Q5)
-        print("You did well but this question gotta be the easiest one ¯\_(ツ)_/¯")
+        print("better luck,Next time :)")
         if score <=0:
             score =0
+#Question 6
             
+    print("\nQuestion: 6 | score: {}".format(score))
+
+    ans=input("Who’s leading the NBA all-time tripple double?\na.Stephen Curry\nb.Oscar Robinson\nc.Lebron James\nd.Russel Westbrook : ")
+    if ans == 'Russel Westbrook' or ans == 'russel westbrook'or ans == 'D' or ans == 'd':
+        print('Correct')
+        score +=1
+    else:
+        print("incorrect it's" ,Q6)
+        print("better luck,Next time :)")
+        if score <=0:
+            score =0
 
 #It's time to calculate the user points
 print(" Your totall score is",score)
-
-
+print("Thanks for trying my quiz feel free to come back soon")
