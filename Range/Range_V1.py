6e172b4268f1ff32f80d8398fb9442204d584294
Yellowgoat1212 <78886208+Yellowgoat1212@users.noreply.github.com>
2021-06-03 22:51:27 +1200

    Update Range_V1.py

--- Range/Range_V1.py
+++ Range/Range_V1.py
@@ -1,10 +1,10 @@
 while True:
     try:
-        number=int(input('How range you want :'))
+        number=int(input('How range you want :'))#Giving the users options to how many questions they want.
         if number>len(nbaquiz) or number<=0:
             print('range 0-'+len(nbaquiz))
         else:
             total=number
             break
     except:
-        print('Place type number')
+        print('Please enter a number from 1-10')#this is if the users make mistake on entering more range than expected.
