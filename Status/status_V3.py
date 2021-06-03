817c7139c09cabeda12b8f73c0e705dbbf9422c6
Yellowgoat1212 <78886208+Yellowgoat1212@users.noreply.github.com>
2021-06-03 13:25:00 +1200

    Add files via upload

---
+++ Status/status_V3.py
@@ -0,0 +1,10 @@
+# what if the user is ready?
+if status == 'Yes' or status == 'yes' or status == 'yeah' or status == 'Y' or status == 'Ye' or status == 'ye' or status == 'A' or status == 'a':
+    print("=================================================================            ")
+    print("------------Welcome to NBA Quiz program-------------------            ")
+    print("=================================================================            ")
+
+# what if the the user is not ready
+else:
+    print("See you next time.")
+    exit()
